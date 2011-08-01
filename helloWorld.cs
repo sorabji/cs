28053469324e91460b6beb096c12bc45b1bdacea
art laptop <sorabji52@gmail.com>
2011-08-01 08:33:50 -0700

    let's see how this works

---
+++ helloWorld.cs
@@ -0,0 +1,8 @@
+using System;
+public class helloWorld{
+    static void Main(){
+        Console.WriteLine("hello world");
+        Console.WriteLine("this fucking sucks");
+        Console.WriteLine("i mean, this is damn awful");
+    }
+}
