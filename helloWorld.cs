ef6fd93807aaf8d31164244eeac3ab2d344286d0
art laptop <sorabji52@gmail.com>
2011-08-01 08:48:33 -0700

    bleck

--- helloWorld.cs
+++ helloWorld.cs
@@ -2,6 +2,6 @@ using System;
 public class helloWorld{
     static void Main(){
         Console.WriteLine("hello world");
-        Console.WriteLine("i mean, this is damn awful");
+        Console.WriteLine("yuck");
     }
 }
